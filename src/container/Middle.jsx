644556bf8c52ae167705e3b9f8d9332f8e307529
kilpatrickap <kilpatrickap18@gmail.com>
2021-03-30 07:54:28 +0000

    Card decks styled with shadow

--- src/container/Middle.jsx
+++ src/container/Middle.jsx
@@ -6,47 +6,54 @@ import Icon_eqpmt from "../assets/eqpmt.png";
 import Icon_lab from "../assets/lab.png";
 import "../styles/middleStyle.css";
 
+const cardStyle = {
+	boxShadow: "5px 8px 24px 5px rgba(208, 216, 243, 0.6)",
+};
+
 function Middle() {
 	return (
 		<div className="center">
 			<CardDeck>
-				<Card border="secondary">
+				<Card border="gray" style={cardStyle}>
 					<Card.Header>MATERIALS</Card.Header>
 					<Image className="card-img" src={Icon_mats} />
 					<Card.Body>
 						<Card.Text>Click here for Materials cost</Card.Text>
 						<Link to="/materials">
-							<Button variant="primary" size="lg" block>Materials</Button>
+							<Button variant="secondary" size="lg" block>
+								Materials
+							</Button>
 						</Link>
-						
 					</Card.Body>
 					<Card.Footer>
 						<small className="text-muted">Last updated 3 mins ago</small>
 					</Card.Footer>
 				</Card>
-				<Card border="secondary">
+				<Card border="gray" style={cardStyle}>
 					<Card.Header>EQUIPMENT</Card.Header>
 					<Card.Img variant="top" src={Icon_eqpmt} />
 					<Card.Body>
-						<Card.Text>Click here for Equipment cost.</Card.Text>
+						<Card.Text>Click here for Equipment cost</Card.Text>
 						<Link to="/equipment">
-							<Button variant="primary" size="lg" block>Equipment</Button>
+							<Button variant="secondary" size="lg" block>
+								Equipment
+							</Button>
 						</Link>
-						
 					</Card.Body>
 					<Card.Footer>
 						<small className="text-muted">Last updated 4 mins ago</small>
 					</Card.Footer>
 				</Card>
-				<Card border="secondary">
+				<Card border="gray" style={cardStyle}>
 					<Card.Header>LABOUR</Card.Header>
 					<Card.Img variant="top" src={Icon_lab} />
 					<Card.Body>
 						<Card.Text>Click here for Labour cost</Card.Text>
 						<Link to="/labour">
-							<Button variant="primary" size="lg" block>Labour</Button>
+							<Button variant="secondary" size="lg" block>
+								Labour
+							</Button>
 						</Link>
-						
 					</Card.Body>
 					<Card.Footer>
 						<small className="text-muted">Last updated 7 mins ago</small>
